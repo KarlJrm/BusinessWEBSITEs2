750dfd2f5a6a0c36e327631c58b9da8a8ca0182a
Jerm <garinjerome43@gmail.com>
2024-05-20 20:27:16 +0800

    Upload All File

---
+++ rtwords.js
@@ -0,0 +1,33 @@
+/*FadeIn Text Effect*/
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        let h1 = entry.target.querySelector('h1');
+        if (entry.isIntersecting) {
+            h1.classList.add('fadeIn');
+        } else {
+            h1.classList.remove('fadeIn');
+        }
+    });
+}, { threshold: [0.5] });
+
+document.querySelectorAll('section').forEach(section => {
+    observer.observe(section);
+});
+
+
+/*Parallax Effect*/
+document.querySelectorAll('section').forEach(section => {
+    section.addEventListener('mousemove', function(e) {
+        let moveX = (e.pageX * -1 / 60);
+        let moveY = (e.pageY * -1 / 60);
+        this.style.backgroundPosition = `calc(50% + ${moveX}px) calc(50% + ${moveY}px)`;
+    });
+});
+
+
+$(document).ready(function(){
+    // Enable dropdowns
+    $('.dropdown-toggle').dropdown();
+});
+
+
